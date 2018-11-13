00d27d0fe46912cb3e0f14898c1e82b5677529ca
jonbaxt <jonbaxt@gmail.com>
2018-11-13 01:49:13 -0700

    added more axios calls to reducer

--- src/ducks/reducer.js
+++ src/ducks/reducer.js
@@ -11,7 +11,14 @@ const initialState = {
 const FULFILLED = '_FULFILLED';
 
 const GET_PRODUCTS_FROM_API = 'GET_PRODUCTS_FROM_API';
-
+const UPDATE_PRODUCT_NAME = 'UPDATE_PRODUCT_NAME';
+const UPDATE_PRODUCT_PART_NUMBER = 'UPDATE_PRODUCT_PART_NUMBER';
+const UPDATE_PRODUCT_LABEL = 'UPDATE_PRODUCT_LABEL';
+const UPDATE_PRODUCT_IMAGE = 'UPDATE_PRODUCT_IMAGE';
+const UPDATE_PRODUCT_CURRENT_COUNT = 'UPDATE_PRODUCT_CURRENT_COUNT';
+const UPDATE_PRODUCT_MINIMUM_STOCK = 'UPDATE_PRODUCT_MINIMUM_STOCK';
+const UPDATE_PRODUCT_PRICE = 'UPDATE_PRODUCT_PRICE';
+const UPDATE_PRODUCT_ALERT_WHEN_LOW = 'UPDATE_PRODUCT_ALERT_WHEN_LOW';
 
 const GET_USERS_FROM_API = 'GET_USERS_FROM_API';
 const GET_ORDERS_FROM_API = 'GET_ORDERS_FROM_API';
@@ -119,6 +126,102 @@ export function updateClientAddressesById(clientId, addressesObject) {
     }
 }
 
+export function updateProductName(productId, productName) {
+    const prodObj = {
+        product_name: productName,
+    }
+    let updatedProductTable = axios.put(`/api/inventoryproducts/updatename/${productId}`, prodObj).then((axiosResults) => { return axiosResults.data; }).catch((err) => console.log(err));
+
+    return {
+        type: UPDATE_PRODUCT_NAME,
+        payload: updatedProductTable
+    }
+}
+
+export function updateProductPartNumber(productId, partNumber) {
+    const prodObj = {
+        part_number: partNumber,
+    }
+    let updatedProductTable = axios.put(`/api/inventoryproducts/updatepartnumber/${productId}`, prodObj).then((axiosResults) => { return axiosResults.data; }).catch((err) => console.log(err));
+
+    return {
+        type: UPDATE_PRODUCT_PART_NUMBER,
+        payload: updatedProductTable
+    }
+}
+
+export function updateProductLabel(productId, productLabel) {
+    const prodObj = {
+        product_label: productLabel,
+    }
+    let updatedProductTable = axios.put(`/api/inventoryproducts/updateproductlabel/${productId}`, prodObj).then((axiosResults) => { return axiosResults.data; }).catch((err) => console.log(err));
+
+    return {
+        type: UPDATE_PRODUCT_LABEL,
+        payload: updatedProductTable
+    }
+}
+
+export function updateProductImage(productId, productImage) {
+    const prodObj = {
+        product_image: productImage,
+    }
+    let updatedProductTable = axios.put(`/api/inventoryproducts/updateproductimage/${productId}`, prodObj).then((axiosResults) => { return axiosResults.data; }).catch((err) => console.log(err));
+
+    return {
+        type: UPDATE_PRODUCT_IMAGE,
+        payload: updatedProductTable
+    }
+}
+
+export function updateProductCurrentCount(productId, currentCount) {
+    const prodObj = {
+        current_count: currentCount,
+    }
+    let updatedProductTable = axios.put(`/api/inventoryproducts/updateproductcurrentcount/${productId}`, prodObj).then((axiosResults) => { return axiosResults.data; }).catch((err) => console.log(err));
+
+    return {
+        type: UPDATE_PRODUCT_CURRENT_COUNT,
+        payload: updatedProductTable
+    }
+}
+
+export function updateProductMinimumStock(productId, minimumStock) {
+    const prodObj = {
+        minimum_stock: minimumStock,
+    }
+    let updatedProductTable = axios.put(`/api/inventoryproducts/updateproductminimumcount/${productId}`, prodObj).then((axiosResults) => { return axiosResults.data; }).catch((err) => console.log(err));
+
+    return {
+        type: UPDATE_PRODUCT_MINIMUM_STOCK,
+        payload: updatedProductTable
+    }
+}
+
+export function updateProductPrice(productId, productPrice) {
+    const prodObj = {
+        price: productPrice,
+    }
+    let updatedProductTable = axios.put(`/api/inventoryproducts/updateproductprice/${productId}`, prodObj).then((axiosResults) => { return axiosResults.data; }).catch((err) => console.log(err));
+
+    return {
+        type: UPDATE_PRODUCT_PRICE,
+        payload: updatedProductTable
+    }
+}
+
+export function updateProductAlertWhenLow(productId, alertWhenLow) {
+    const prodObj = {
+        alert_when_low: alertWhenLow,
+    }
+    let updatedProductTable = axios.put(`/api/inventoryproducts/updateproductalertwhenlow/${productId}`, prodObj).then((axiosResults) => { return axiosResults.data; }).catch((err) => console.log(err));
+
+    return {
+        type: UPDATE_PRODUCT_ALERT_WHEN_LOW,
+        payload: updatedProductTable
+    }
+}
+
 export function deleteClientById(clientId) {
     let newClientArray = axios.delete(`/api/delete/client/byid/${clientId}`).then((axiosResults) => { return axiosResults.data }).catch((err) => console.log(err));
 
@@ -155,13 +258,32 @@ export default function reducer(state = initialState, action) {
     switch (action.type) {
         case GET_PRODUCTS_FROM_API + FULFILLED:
             return Object.assign({}, state, { productsArray: action.payload });
-        case GET_CLIENTS_FROM_API + FULFILLED:
-            return Object.assign({}, state, { clientsArray: action.payload });
+
+        case UPDATE_PRODUCT_NAME + FULFILLED:
+            return Object.assign({}, state, { productsArray: action.payload });
+        case UPDATE_PRODUCT_PART_NUMBER + FULFILLED:
+            return Object.assign({}, state, { productsArray: action.payload });
+        case UPDATE_PRODUCT_LABEL + FULFILLED:
+            return Object.assign({}, state, { productsArray: action.payload });
+        case UPDATE_PRODUCT_IMAGE + FULFILLED:
+            return Object.assign({}, state, { productsArray: action.payload });
+        case UPDATE_PRODUCT_CURRENT_COUNT + FULFILLED:
+            return Object.assign({}, state, { productsArray: action.payload });
+        case UPDATE_PRODUCT_MINIMUM_STOCK + FULFILLED:
+            return Object.assign({}, state, { productsArray: action.payload });
+        case UPDATE_PRODUCT_PRICE + FULFILLED:
+            return Object.assign({}, state, { productsArray: action.payload });
+        case UPDATE_PRODUCT_ALERT_WHEN_LOW + FULFILLED:
+            return Object.assign({}, state, { productsArray: action.payload });
+
+
         case GET_USERS_FROM_API + FULFILLED:
             return Object.assign({}, state, { usersArray: action.payload });
         case GET_ORDERS_FROM_API + FULFILLED:
             return Object.assign({}, state, { ordersArray: action.payload });
 
+        case GET_CLIENTS_FROM_API + FULFILLED:
+            return Object.assign({}, state, { clientsArray: action.payload });
         case POST_NEW_CLIENT + FULFILLED:
             return Object.assign({}, state, { clientsArray: action.payload });
         case UPDATE_CLIENT_NAME_BY_ID + FULFILLED:
